c23128c63847bdc811075e67b71052fe168d6979
Indy Naessens <indy.naessens@protonmail.com>
2019-11-03 22:31:16 +0100

    Tests pass
    - See comments for :time.sleep increases and why that is

--- 01-basic-crypto-cloner/student/test/assignment_one_test.exs
+++ 01-basic-crypto-cloner/student/test/assignment_one_test.exs
@@ -36,22 +36,30 @@ defmodule AssignmentOneTest do
     |> Process.exit(:kill)
 
     assert ProcessManager.retrieve_coin_processes() |> length() == amount_of_processes
+
+    :timer.sleep(2000)
+
+    assert ProcessManager.retrieve_coin_processes()
+           |> Enum.all?(fn {_, pid} -> Process.alive?(pid) == true end) == true
   end
 
   # Tests for Logger
   test "Logger can print" do
-    Logger.log("LOGGER TEST, If you see this put value to true")
+    Logger.log("\n\n\nLOGGER TEST, If you see this put value to true\n\n\n")
 
     assert true
   end
 
   # Tests for RateLimiter
-  test "RateLimiter its value can be changed" do
-    Logger.log("RATE TEST, If you see the speed of the requests go up after ~5 seconds, put value to true")
+  test "\n\n\nRateLimiter its value can be changed" do
+    Logger.log(
+      "\n\n\nRATE TEST, If you see the speed of the requests go up after ~5 seconds, put value to true\n\n\n"
+    )
 
     RateLimiter.change_rate_limit(1)
     :timer.sleep(5000)
     RateLimiter.change_rate_limit(5)
+    Logger.log("\n\n\n")
     :timer.sleep(3000)
     assert true
   end
@@ -68,6 +76,11 @@ defmodule AssignmentOneTest do
     # Test are run in random order every time so sometimes coin data isn't there yet etc
     :timer.sleep(2000)
 
+    Logger.log("\n\n\nStarting CoinDataRetriever test, this wil take ~2 mins. Why?
+    \nThis time is so long because when we split the time frame and ask the ratelimiter for permission it adds us at the end of the queue.
+    \nWhen a request comes back with 1000 trades we don't add any of it to the history we just split it and do 2 requests with a smaller timeframe.
+    \n\n\n")
+
     pid =
       AssignmentOne.ProcessManager.retrieve_coin_processes()
       |> Enum.filter(fn {coin, _pid} -> coin == "BTC_DGB" end)
@@ -75,7 +88,11 @@ defmodule AssignmentOneTest do
       |> elem(1)
 
     length_old = CoindataRetriever.get_history(pid) |> elem(1) |> length
-    :timer.sleep(60_000)
+
+    # this time is so long because when we split the time frame and ask the ratelimiter for permission it adds us at the end of the queue
+    # queue is first in first out but the ratelimiter only knows pids so when a coin does a request and gets back 1000 results
+    # it will ask the ratelimiter for 2 new requests but has bad luck and is at the end of the queue now
+    :timer.sleep(120_000)
     length_new = CoindataRetriever.get_history(pid) |> elem(1) |> length
     assert length_new > length_old
   end
