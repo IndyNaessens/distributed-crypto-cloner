89da1a0ff133aff7c8f81d4e6544e592ef8a80d7
Indy Naessens <indy.naessens@protonmail.com>
2019-12-07 20:13:44 +0100

    timeframe 33 days

--- 01-basic-crypto-cloner/student/config/config.exs
+++ 01-basic-crypto-cloner/student/config/config.exs
@@ -1,6 +1,6 @@
 import Config
 
 config :assignment,
-  from: (DateTime.utc_now() |> DateTime.to_unix()) - 60 * 60 * 24 * 1,
+  from: (DateTime.utc_now() |> DateTime.to_unix()) - 60 * 60 * 24 * 33,
   until: DateTime.utc_now() |> DateTime.to_unix(),
   rate: 5
