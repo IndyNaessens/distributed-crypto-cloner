7095015464f732246815a42c3fcb9653d59d9ab7
Indy Naessens <indy.naessens@protonmail.com>
2020-01-15 18:08:40 +0100

    Balancing done

--- 01-basic-crypto-cloner/student/lib/assignment/coindata_coordinator.ex
+++ 01-basic-crypto-cloner/student/lib/assignment/coindata_coordinator.ex
@@ -19,6 +19,14 @@ defmodule Assignment.CoindataCoordinator do
     GenServer.call(__MODULE__, :get_history_keeper_worker_statistics)
   end
 
+  def get_history_keeper_worker_statistic(coin_name) do
+    GenServer.call(__MODULE__, {:get_history_keeper_worker_statistic, coin_name})
+  end
+
+  def get_hist_keeper_coin_pairs() do
+    GenServer.call(__MODULE__, :get_hist_keeper_coin_pairs)
+  end
+
   @doc """
   Transfers a HistoryKeeperWorker and CoindataRetriever responsible for the given coin_name
   from the current node to the node_to
@@ -28,7 +36,7 @@ defmodule Assignment.CoindataCoordinator do
   end
 
   def balance() do
-
+    GenServer.cast(__MODULE__, :balance)
   end
 
   # SERVER
@@ -41,17 +49,107 @@ defmodule Assignment.CoindataCoordinator do
     # expects the short name of the reporter node to contain "reporter"
     amount_of_cloner_nodes =
       Node.list()
-      |> Enum.map(&(Atom.to_string(&1) |> String.split("@") |> List.first() |> String.downcase()))
-      |> Enum.filter(&(!String.contains?(&1, "reporter")))
+      |> Enum.filter(
+        &(Atom.to_string(&1) |> String.downcase() |> String.contains?("reporter") == false)
+      )
       |> length
 
     # when we have zero cloner nodes, start cloning as first node
     if amount_of_cloner_nodes == 0 do
       Assignment.PoloniexAPiCaller.return_ticker()
       |> Map.keys()
-      |> Enum.take(25)
+      #|> Enum.take(30)
       |> Enum.each(&add_coin(&1))
     end
+
+    {:noreply, state}
+  end
+
+  def handle_cast(:balance, state) do
+    # nodes without ourself
+    nodes =
+      Node.list()
+      # only get worker nodes
+      |> Enum.filter(
+        &(Atom.to_string(&1) |> String.downcase() |> String.contains?("reporter") == false)
+      )
+
+    # all nodes, ourself included (no reporter)
+    nodes_all = [Node.self(), nodes] |> List.flatten()
+
+    coins_being_handled =
+      nodes
+      # for each node get a list of the coin pairs for the history keeper workers
+      |> Enum.map(fn node ->
+        {node,
+         GenServer.call({Assignment.CoindataCoordinator, node}, :get_hist_keeper_coin_pairs)}
+      end)
+      # include ourself
+      |> Enum.concat(Assignment.HistoryKeeperManager.retrieve_history_processes())
+      # map to {node, coin} so we now which node handles wich coin
+      |> Enum.map(fn {node, list} -> Enum.map(list, fn pair -> {node, elem(pair, 0)} end) end)
+      # flatten the list so we have 1 list instead of N node lists in a list
+      |> List.flatten()
+
+    # we want all supported coins from the rest service
+    unhandeled_coins =
+      Assignment.PoloniexAPiCaller.return_ticker()
+      |> Map.keys()
+      # we only want the coins that are not present in the handled coins
+      |> Enum.filter(
+        &(!Enum.member?(coins_being_handled |> Enum.map(fn pair -> elem(pair, 1) end), &1))
+      )
+      # map it to the right format {node, coin} (node becomes nil because it's not handled by any node)
+      |> Enum.map(&{nil, &1})
+
+    # combine both lists so we have a nice overview of which coin is handled by wich node or if it's not handled by any node
+    combined_list = [coins_being_handled | unhandeled_coins] |> List.flatten()
+
+    # zip it so we know wich coin is going to wich new node
+    # we get a list if this type {{node, coin}, new_node}
+    # we do +1 because if we have 92 coins and 3 nodes, div(92,3) is 30
+    # if we duplicate a list of 3 nodes 30 times and flatten it we have a list of 90 items
+    # but we have 92 coins so we do +1 so we have 93 items and then zip it the last one doesn't matter
+    # because zip finishes when any enumarable completes and the combined list completes first
+    combined_list
+    # sort on progress for the balancing
+    |> Enum.sort_by(fn {node_from, coin_name} ->
+      cond do
+        node_from == Node.self() ->
+          Assignment.HistoryKeeperManager.get_pid_for(coin_name)
+          |> Assignment.HistoryKeeperWorker.get_statistics()
+          |> Map.get(:progress)
+
+        node_from != nil ->
+          GenServer.call(
+            {Assignment.CoindataCoordinator, node_from},
+            {:get_history_keeper_worker_statistic, coin_name}
+          )
+          |> Map.get(:progress)
+
+        true ->
+          0.00
+      end
+    end)
+    |> Enum.zip(
+      List.duplicate(nodes_all, div(combined_list |> length, nodes_all |> length) + 1)
+      |> List.flatten()
+    )
+    # do the balancing
+    # if the coin is being handled by a node, transfer it
+    # if the coin is not handled by a node, start it
+    |> Enum.each(fn coin_info_for_balancing ->
+      case coin_info_for_balancing do
+        {{nil, coin_name}, node_to} ->
+          GenServer.cast({Assignment.CoindataCoordinator, node_to}, {:add_coin, coin_name})
+
+        {{node_from, coin_name}, node_to} ->
+          GenServer.cast(
+            {Assignment.CoindataCoordinator, node_from},
+            {:transfer_coin, coin_name, node_to}
+          )
+      end
+    end)
 
     {:noreply, state}
   end
@@ -94,11 +192,21 @@ defmodule Assignment.CoindataCoordinator do
     {:noreply, state}
   end
 
+  def handle_call(:get_hist_keeper_coin_pairs, _from, state) do
+    {:reply, Assignment.HistoryKeeperManager.retrieve_history_processes(), state}
+  end
+
   def handle_call(:get_history_keeper_worker_statistics, _from, state) do
     pairs =
       Assignment.HistoryKeeperManager.retrieve_history_processes()
       |> Enum.map(fn {_coin, pid} -> Assignment.HistoryKeeperWorker.get_statistics(pid) end)
 
     {:reply, pairs, state}
+  end
+
+  def handle_call({:get_history_keeper_worker_statistic, coin_name}, _from, state) do
+    {:reply,
+     Assignment.HistoryKeeperManager.get_pid_for(coin_name)
+     |> Assignment.HistoryKeeperWorker.get_statistics(), state}
   end
 end
