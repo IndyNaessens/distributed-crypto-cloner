187f46652094057d9b9cb6b21be0cdbcd2db14bc
Indy Naessens <indy.naessens@protonmail.com>
2020-01-15 19:08:33 +0100

    Coordinator is now global

--- 01-basic-crypto-cloner/student/lib/assignment/coindata_coordinator.ex
+++ 01-basic-crypto-cloner/student/lib/assignment/coindata_coordinator.ex
@@ -3,28 +3,34 @@ defmodule Assignment.CoindataCoordinator do
 
   # API
   def start_link([]) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    GenServer.start_link(__MODULE__, [], name: {:global, {Node.self(), __MODULE__}})
   end
 
   def add_coin(coin_name) when is_binary(coin_name) do
-    GenServer.cast(__MODULE__, {:add_coin, coin_name})
+    GenServer.cast({:global, {Node.self(), __MODULE__}}, {:add_coin, coin_name})
   end
 
   def add_coin(coin_name, history_keeper_worker_state)
       when is_map(history_keeper_worker_state) and is_binary(coin_name) do
-    GenServer.cast(__MODULE__, {:add_coin, coin_name, history_keeper_worker_state})
+    GenServer.cast(
+      {:global, {Node.self(), __MODULE__}},
+      {:add_coin, coin_name, history_keeper_worker_state}
+    )
   end
 
   def get_history_keeper_worker_statistics() do
-    GenServer.call(__MODULE__, :get_history_keeper_worker_statistics)
+    GenServer.call({:global, {Node.self(), __MODULE__}}, :get_history_keeper_worker_statistics)
   end
 
   def get_history_keeper_worker_statistic(coin_name) do
-    GenServer.call(__MODULE__, {:get_history_keeper_worker_statistic, coin_name})
+    GenServer.call(
+      {:global, {Node.self(), __MODULE__}},
+      {:get_history_keeper_worker_statistic, coin_name}
+    )
   end
 
   def get_hist_keeper_coin_pairs() do
-    GenServer.call(__MODULE__, :get_hist_keeper_coin_pairs)
+    GenServer.call({:global, {Node.self(), __MODULE__}}, :get_hist_keeper_coin_pairs)
   end
 
   @doc """
@@ -32,11 +38,11 @@ defmodule Assignment.CoindataCoordinator do
   from the current node to the node_to
   """
   def transfer_coin(coin_name, node_to) do
-    GenServer.cast(__MODULE__, {:transfer_coin, coin_name, node_to})
+    GenServer.cast({:global, {Node.self(), __MODULE__}}, {:transfer_coin, coin_name, node_to})
   end
 
   def balance() do
-    GenServer.cast(__MODULE__, :balance)
+    GenServer.cast({:global, {Node.self(), __MODULE__}}, :balance)
   end
 
   # SERVER
@@ -58,7 +64,7 @@ defmodule Assignment.CoindataCoordinator do
     if amount_of_cloner_nodes == 0 do
       Assignment.PoloniexAPiCaller.return_ticker()
       |> Map.keys()
-      #|> Enum.take(30)
+      # |> Enum.take(30)
       |> Enum.each(&add_coin(&1))
     end
 
@@ -82,7 +88,10 @@ defmodule Assignment.CoindataCoordinator do
       # for each node get a list of the coin pairs for the history keeper workers
       |> Enum.map(fn node ->
         {node,
-         GenServer.call({Assignment.CoindataCoordinator, node}, :get_hist_keeper_coin_pairs)}
+         GenServer.call(
+           {:global, {node, __MODULE__}},
+           :get_hist_keeper_coin_pairs
+         )}
       end)
       # include ourself
       |> Enum.concat(Assignment.HistoryKeeperManager.retrieve_history_processes())
@@ -105,12 +114,6 @@ defmodule Assignment.CoindataCoordinator do
     # combine both lists so we have a nice overview of which coin is handled by wich node or if it's not handled by any node
     combined_list = [coins_being_handled | unhandeled_coins] |> List.flatten()
 
-    # zip it so we know wich coin is going to wich new node
-    # we get a list if this type {{node, coin}, new_node}
-    # we do +1 because if we have 92 coins and 3 nodes, div(92,3) is 30
-    # if we duplicate a list of 3 nodes 30 times and flatten it we have a list of 90 items
-    # but we have 92 coins so we do +1 so we have 93 items and then zip it the last one doesn't matter
-    # because zip finishes when any enumarable completes and the combined list completes first
     combined_list
     # sort on progress for the balancing
     |> Enum.sort_by(fn {node_from, coin_name} ->
@@ -122,7 +125,7 @@ defmodule Assignment.CoindataCoordinator do
 
         node_from != nil ->
           GenServer.call(
-            {Assignment.CoindataCoordinator, node_from},
+            {:global, {node_from, __MODULE__}},
             {:get_history_keeper_worker_statistic, coin_name}
           )
           |> Map.get(:progress)
@@ -131,6 +134,12 @@ defmodule Assignment.CoindataCoordinator do
           0.00
       end
     end)
+    # zip it so we know wich coin is going to wich new node
+    # we get a list if this type {{node, coin}, new_node}
+    # we do +1 because if we have 92 coins and 3 nodes, div(92,3) is 30
+    # if we duplicate a list of 3 nodes 30 times and flatten it we have a list of 90 items
+    # but we have 92 coins so we do +1 so we have 93 items and then zip it the last one doesn't matter
+    # because zip finishes when any enumarable completes and the combined list completes first
     |> Enum.zip(
       List.duplicate(nodes_all, div(combined_list |> length, nodes_all |> length) + 1)
       |> List.flatten()
@@ -141,11 +150,11 @@ defmodule Assignment.CoindataCoordinator do
     |> Enum.each(fn coin_info_for_balancing ->
       case coin_info_for_balancing do
         {{nil, coin_name}, node_to} ->
-          GenServer.cast({Assignment.CoindataCoordinator, node_to}, {:add_coin, coin_name})
+          GenServer.cast({:global, {node_to, __MODULE__}}, {:add_coin, coin_name})
 
         {{node_from, coin_name}, node_to} ->
           GenServer.cast(
-            {Assignment.CoindataCoordinator, node_from},
+            {:global, {node_from, __MODULE__}},
             {:transfer_coin, coin_name, node_to}
           )
       end
@@ -185,7 +194,7 @@ defmodule Assignment.CoindataCoordinator do
 
     # start keeper with state and start retriever on new node
     GenServer.cast(
-      {Assignment.CoindataCoordinator, node_to},
+      {:global, {node_to, __MODULE__}},
       {:add_coin, coin_name, state_from_keeper}
     )
 
--- reporter/lib/reporter/reporter.ex
+++ reporter/lib/reporter/reporter.ex
@@ -18,9 +18,10 @@ defmodule Assignment.Reporter do
   end
 
   def handle_info(:render_table, state) do
+
     Node.list()
     |> Enum.map(
-      &GenServer.call({Assignment.CoindataCoordinator, &1}, :get_history_keeper_worker_statistics)
+      &GenServer.call(:global.whereis_name({&1, Assignment.CoindataCoordinator}), :get_history_keeper_worker_statistics)
     )
     |> List.flatten()
     |> Enum.sort_by(&Map.get(&1, :progress))
